b68c4c88668b2520d1f2a821041897c0612ce721
Vinicius Maeda <viniciusmaeda@gmail.com>
2019-09-24 15:40:42 -0400

    Adicionado comentários

--- app/src/main/java/com/maeda/exemploradiobutton/MainActivity.java
+++ app/src/main/java/com/maeda/exemploradiobutton/MainActivity.java
@@ -1,17 +1,17 @@
 package com.maeda.exemploradiobutton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // define objtetos relacionados às views da activity
     private RadioGroup grupoDeOpcoes;
     private RadioButton radioOpcao;
     private TextView textoOpcaoEscolhida;
@@ -22,15 +22,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // instancia as views
         grupoDeOpcoes = findViewById(R.id.rdg_grupo_de_opcoes);
         textoOpcaoEscolhida = findViewById(R.id.txv_opcao_escolhida);
         botaoMostrarOpcao = findViewById(R.id.btn_mostrar_opcao);
 
+        // listener para capturar o evento OnClick
         botaoMostrarOpcao.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // obtém o ID do item selecionado
                 int opcao = grupoDeOpcoes.getCheckedRadioButtonId();
+                // obtém os dados do RadioButton selecionado
                 radioOpcao = findViewById(opcao);
+                // mostra o texto do RadioButton numa TextView
                 textoOpcaoEscolhida.setText("Opção escolhida: " + radioOpcao.getText());
             }
         });
